25bbf5ba0612fb6ff6942c6c1bee7f1d098871c3
intvoker <intvoker@gmail.com>
2021-12-22 22:40:42 -0800

    Initial commit

---
+++ Source/Mials/Mials.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Mials.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Mials, "Mials" );
---
+++ Source/Mials/MialsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MialsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIALS_API AMialsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
